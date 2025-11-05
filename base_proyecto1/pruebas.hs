e93fa455f6acfc1b2636da6fb8432807800c8d7d
arcargotte <alan.argotte@gmail.com>
2025-11-05 02:04:17 -0400

    Feat: created game loop and finished first draft of command parser

--- base_proyecto1/pruebas.hs
+++ base_proyecto1/pruebas.hs
@@ -2,19 +2,28 @@ import System.IO (hFlush, stdout)
 
 data Command = Command {
     instruction :: String,
-    param :: Maybe String
+    param :: String
 }
 
 parseCommand :: String -> Maybe Command
 parseCommand s = case splitCommand (==' ') s of
     [] -> Nothing
-    (x:xs) -> case x of
-        "ir" -> Just Command { instruction = "ir", param = xs}
-        "mirar" -> Just Command { instruction = "mirar", param = Nothing}
-        "tomar" -> Just Command { instruction = "tomar", param = xs}
-        "inventario" -> Just Command { instruction = "inventario"}
-        "salir" -> Just Command { instruction = "salir", param = Nothing}
-        _ -> Nothing
+    (x:xs) -> 
+        let params = unwords xs
+        in case (x, params) of
+            ("ir", "norte ") -> Just Command { instruction = "ir", param = params}
+            ("ir", "este ") -> Just Command { instruction = "ir", param = params}
+            ("ir", "sur ") -> Just Command { instruction = "ir", param = params}
+            ("ir", "oeste ") -> Just Command { instruction = "ir", param = params}
+            ("ir", _) -> Nothing
+            ("mirar", "") -> Just Command { instruction = "mirar", param = params}
+            ("mirar", _) -> Nothing
+            ("tomar", _) -> Just Command { instruction = "tomar", param = params}
+            ("inventario", "") -> Just Command { instruction = "inventario", param = params}
+            ("inventario", _) -> Nothing
+            ("salir", "") -> Just Command { instruction = "salir", param = params}
+            ("salir", _) -> Nothing
+            _ -> Nothing
 
 main :: IO ()
 main = gameloop
@@ -26,17 +35,32 @@ splitCommand p s = case dropWhile p s of
     s' -> w : splitCommand p s'' 
         where (w, s'') = break p s'
 
+invalidCommand :: IO()
+invalidCommand = do
+    putStrLn("Instrucción no reconocida")
+    gameloop
+
 gameloop :: IO()
 gameloop = do
     putStrLn "Ingrese un comando"
     putStr "> "
     hFlush stdout
     command <- getLine
-    -- let entryList = splitCommand (==' ') command
-    -- let firstCommand = head entryList
+    let entryList = splitCommand (==' ') command
+    let params = tail entryList
     let cmd = parseCommand command
+    -- case cmd of
+    --     Just (Command "ir" params) -> putStrLn ("Envió como primer comando a IR " ++ params)
+    --     Just (Command "mirar" params) -> putStrLn ("Envió como primer comando a MIRAR " ++ params)
+    --     Just (Command "tomar" params) -> putStrLn ("Envió como primer comando a TOMAR " ++ params)
+    --     Just (Command "inventario" params) -> putStrLn ("Envió como primer comando a INVENTARIO " ++ params)
+    --     Just (Command "salir" params) -> putStrLn ("Envió como primer comando a SALIR " ++ params)
+    --     Nothing -> putStrLn ("Instrucción no reconocida")
     case cmd of
-        Just (Command "tomar") -> putStrLn ("Envió como primer comando a TOMAR")
-        Just (Command "ir") -> putStrLn ("Envió como primer comando a IR")
-        Just (Command "salir") -> putStrLn ("Envió como primer comando a SALIR")
-        Nothing -> putStrLn ("Instrucción no reconocida") 
+        Just (Command "ir" params) -> gameloop --Crear función que procese la lógica de IR que llame luego a gameloop
+        Just (Command "mirar" params) -> gameloop --Crear función que procese la lógica de MIRAR que llame luego a gameloop
+        Just (Command "tomar" params) -> gameloop --Crear función que procese la lógica de TOMAR que llame luego a gameloop
+        Just (Command "inventario" params) -> gameloop --Crear función que procese la lógica de INVENTARIO que llame luego a gameloop
+        Just (Command "salir" params) -> putStrLn ("Saliendo del juego... ")
+        Nothing -> invalidCommand
+
